03df490cfd67cbebafd251d015d8ec66893cb50b
chintan <patelchintan843@gmail.com>
2023-09-06 20:18:30 +0530

    navigate route inside route

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  // active route is use to get the active route
+  constructor(private route: Router, private activeRoute: ActivatedRoute) { }
+  redirect() {
+    // Path for this one --> http://localhost:4200/About/Home
+    this.route.navigate(['Home'], { relativeTo: this.activeRoute })
+  }
+  home() {
+    // Path for this one --> http://localhost:4200/Home
+
+    this.route.navigateByUrl('/Home')
+    // this.route.navigateByUrl('Home') --> same as above
+  }
 }

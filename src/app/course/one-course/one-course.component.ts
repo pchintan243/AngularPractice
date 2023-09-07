745440fcb350ea67bff0ff2c0c6d6a6c4fe9692b
chintan <patelchintan843@gmail.com>
2023-09-07 15:27:32 +0530

    implement another route

--- src/app/course/one-course/one-course.component.ts
+++ src/app/course/one-course/one-course.component.ts
@@ -1,15 +1,16 @@
 import { CoursesService } from './../../Service/courses.service';
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-one-course',
   templateUrl: './one-course.component.html',
   styleUrls: ['./one-course.component.css']
 })
-export class OneCourseComponent implements OnInit {
+export class OneCourseComponent implements OnInit, OnDestroy {
   course: any;
   courseId: any;
+  routeParams: any;
 
   constructor(private ActivatedRoute: ActivatedRoute, private CoursesService: CoursesService) { }
 
@@ -17,9 +18,21 @@ export class OneCourseComponent implements OnInit {
     // To get the id we use snapshot property it retuns the id.
     // this.courseId = this.ActivatedRoute.snapshot.params['id'];
     // --> this works same as above one.
-    this.courseId = this.ActivatedRoute.snapshot.paramMap.get('id');
+    // this.courseId = this.ActivatedRoute.snapshot.paramMap.get('id');
+    // Make sure you use snapshot property when value must not be changed after invoke first time.
 
     // Based on id we get the course details from an course service file.
-    this.course = this.CoursesService.courses.find(x => x.id == this.courseId);
+    // this.course = this.CoursesService.courses.find(x => x.id == this.courseId);
+
+    // let's do it using ovservable 
+    // observable is useful for if we route inside the route.
+    this.routeParams = this.ActivatedRoute.paramMap.subscribe((val) => {
+      this.courseId = val.get('id')
+      this.course = this.CoursesService.courses.find(x => x.id == this.courseId);
+    })
+  }
+  
+  ngOnDestroy() {
+    this.routeParams.unsubscribe();
   }
 }
